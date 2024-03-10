2e27e5d89a687f2f53624c79579187b8538ed956
manish9617 <manish626591@gmail.com>
2024-03-10 19:43:58 +0530

    update user panel

--- frontend/src/Components/UserPanel/UserDetils.jsx
+++ frontend/src/Components/UserPanel/UserDetils.jsx
@@ -1,56 +1,54 @@
 import React, { useState } from "react";
 import styles from "./UserDetils.module.css";
 import Skills from "./Skills";
-import Education from "./Education";
-import Experience from "./Experience";
-import axios from "axios";
+import UserEducation from "./UserEducation";
 
-function UserDetils({tab}) {
+import axios from "axios";
+import UserExperience from "./UserExperience";
+
+function UserDetils({ tab }) {
   const [name, setname] = useState();
   const [add, setadd] = useState();
   const [cont, setcont] = useState();
-     axios.get("",(req,res)=>
-     {
-        
-     })
+  axios.get("", (req, res) => {});
 
   return (
     <div className={styles.main}>
-      {
-        tab==="profile" && <div className={styles.pro}>
-        <h2 className="text-4xl font-bold">Profile Information</h2>
-        <hr />
-        <div className={styles.profile}>
-          <div className={styles.left}>
-            <img src="bhalot.jpg" className={styles.img} />
-            <h2>Name: bhalot</h2>
-            <hr></hr>
-            <button className={`${styles.resume} mt-6 ml-5 p-1`}>
-              View Resume
-            </button>
-          </div>
-          <div className={styles.line}></div>
+      {tab === "profile" && (
+        <div className={styles.pro}>
+          <h2 className="text-4xl font-bold">Profile Information</h2>
           <hr />
-          <div className={styles.right}>
-            <div className={styles.rightUp}>
-              <h3>Address:bhopal</h3>
-              <h3>Contact:9993432646</h3>
-              <label>
-                <h3>Current Status:</h3>
-              </label>
-              <button className={styles.nextbtn}>
-                <h3>Student / Fresher / Employee</h3>
+          <div className={styles.profile}>
+            <div className={styles.left}>
+              <img src="bhalot.jpg" className={styles.img} />
+              <h2>Name: bhalot</h2>
+              <hr></hr>
+              <button className={`${styles.resume} mt-6 ml-5 p-1`}>
+                View Resume
               </button>
             </div>
+            <div className={styles.line}></div>
             <hr />
+            <div className={styles.right}>
+              <div className={styles.rightUp}>
+                <h3>Address:bhopal</h3>
+                <h3>Contact:9993432646</h3>
+                <label>
+                  <h3>Current Status:</h3>
+                </label>
+                <button className={styles.nextbtn}>
+                  <h3>Student / Fresher / Employee</h3>
+                </button>
+              </div>
+              <hr />
+            </div>
           </div>
         </div>
-      </div>
-      }
+      )}
 
-      {tab==="skills" && <Skills></Skills>}
-      {tab==="education" && <Education></Education>}
-      {tab==="experience" && <Experience></Experience>}
+      {tab === "skills" && <Skills></Skills>}
+      {tab === "education" && <UserEducation></UserEducation>}
+      {tab === "experience" && <UserExperience></UserExperience>}
     </div>
   );
 }
---
+++ frontend/src/Components/UserPanel/UserExperience.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./UserDetils.module.css";
+
+function UserExperience() {
+  return (
+    <div>
+      <div className={styles.SecondPro}>
+        <h3>Experience</h3>
+        <hr />
+        <label>Post Graduation</label>
+        <h4>College-National Institiude of Technology Bhopal MCA</h4>
+        <label>Under Graduation</label>
+        <h4>Vikram University Ujjain madhya pradesh</h4>
+        <button className="bg-black p-2 text-xl text-white rounded-[10px]">
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UserExperience;
